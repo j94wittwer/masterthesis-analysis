c190ce8d5eaadd09e451783de06d83d43bcc53e0
j94wittwer <j94wittwer@gmail.com>
2023-08-10 07:07:13 +0200

    minor fix

--- linearity_analysis.py
+++ linearity_analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 import control_flow_path
 import scan_path
@@ -20,6 +21,40 @@ for aoi_id, position in enumerate(ideal_sequence):
     }
     aoi_dict[position] = aoi_attributes
 
-# Print the resulting dictionary
-for aoi_id, attributes in aoi_dict.items():
-    print(f"AOI {aoi_id} Attributes: {attributes}")
+
+class_transitions = np.zeros((4,4), dtype=int)
+
+# Analyze each participant's sequence
+for participant, sequence in participants_sequences.items():
+
+    print(participant)
+
+    # Calculate average direction
+    average_direction = 0
+    for i in range(len(sequence) - 1):
+        if sequence[i] < sequence[i + 1]:
+            average_direction += 1
+        elif sequence[i] > sequence[i + 1]:
+            average_direction -= 1
+    average_direction /= len(sequence) - 1  # Normalize
+
+    # Calculate class transitions
+    for i in range(len(sequence) - 1):
+        start_class = aoi_dict[sequence[i]]['class']
+        target_class = aoi_dict[sequence[i + 1]]['class']
+        start_index = ['CONTROLLER', 'SERVICE', 'USER', 'REPOSITORY'].index(start_class)
+        target_index = ['CONTROLLER', 'SERVICE', 'USER', 'REPOSITORY'].index(target_class)
+        class_transitions[start_index, target_index] += 1
+
+    # Calculate regression rate
+    backward_transitions = sum(1 for i in range(len(sequence) - 1) if sequence[i] > sequence[i + 1])
+    regression_rate = backward_transitions / (len(sequence) - 1) * 100
+
+    # Print results for the participant
+    print(f"Participant: {participant}")
+    print(f"Average Direction: {average_direction}")
+    print("Class Transitions Matrix:")
+    print(class_transitions)
+    print(f"Regression Rate: {regression_rate:.2f}%")
+    print("=" * 40)
+
